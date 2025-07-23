2e5e383fb7025e95b7c65f8e467e8db022f202e4
RileyAlderson <rileymin137@gmail.com>
2025-07-23 10:37:39 -0400

    got rid of comment

--- DietBehavior.java
+++ DietBehavior.java
@@ -3,11 +3,4 @@ public interface DietBehavior
 
     public void eat();
    
-    //public void carnivore();
-
-    //public void herbivore();
-
-    //public void omnivore();
-
-    //public void insectivore();
 }
