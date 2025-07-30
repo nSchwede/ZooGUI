380117ac8d8329ef7e24e25990151ac8c840d3f8
nSchwede <97040865+nSchwede@users.noreply.github.com>
2025-07-30 10:00:05 -0400

    Update CreepyLaugh.java
    altered to fit use case

--- CreepyLaugh.java
+++ CreepyLaugh.java
@@ -2,9 +2,10 @@ public class CreepyLaugh implements SoundBehavior
 {
     //The creepylaugh class implements the interface soundBehavior to display the sound that this specified animal makes.
     @Override
-    public void sound()
+    public string sound()
     {
         //https://youtu.be/6pxSo_aEu10?si=Z31Mh3buob7dsQWO 
         // this is used for a hyena
+        return "hahahahahahahaha";
     }
 }
